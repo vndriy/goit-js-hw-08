5b1b22ac33376a0c44c7e66b9e5ee3c03481297a
vndriy <131777753+vndriy@users.noreply.github.com>
2023-09-15 13:36:25 +0000

    Deploying to gh-pages from @ vndriy/goit-js-hw-08@32cfcca9d05f38a53dea3e08aaed3e91b56bdaf7 🚀

---
+++ 03-feedback.742ec00e.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector('input[type="email"]');localStorage.setItem("feedback-form-state",JSON.stringify(e))}();
+//# sourceMappingURL=03-feedback.742ec00e.js.map
---
+++ 03-feedback.a0fd280f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector('input[type="email"]');localStorage.setItem("feedback-form-state",JSON.stringify(e));
+//# sourceMappingURL=03-feedback.a0fd280f.js.map
